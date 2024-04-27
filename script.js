83a3a7a5035d2a261584beeb2d106d2cfc4e4332
Abdul Momin Khan <Khanabdulmomin24@gmail.com>
2024-04-27 15:51:24 +0530

    The bugs such as forward backward buttons which does not showing the card in recently played and not forwarding in the same section such as trending or featuring.

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ audio.preload="metadata";
 let heart = document.querySelector(".heart");
 let playerControls = document.querySelector(".player-controls");
 let playing = audio.play();
-
 let recentlyAdded=[];
 
 let arr=[
@@ -25,12 +24,14 @@ let arr=[
         image:"./assets/images/jale.jpg",
         Songurl:"./assets/music/Jale2.mp3",
         cardinfo:"this song is of animal movie",
+        SongNo:"1",
     },
     {
         songName:"Pehle bhi main",
         image:"./assets/images/animal4.png",
         Songurl:"./assets/music/PehleBhiMain.mp3",
         cardinfo:"this song is of animal movie",
+        SongNo:"2",
    },
 
     {
@@ -38,12 +39,14 @@ let arr=[
         image:"./assets/images/Tuhai.jpeg",
         Songurl:"./assets/music/TuHai.mp3",
         cardinfo:"This is one of the top trending album",
+        SongNo:"3",
     },
     {
         songName:"Bhaag D.K.Bose",
         image:"./assets/images/BhaagDKB.jpeg",
         Songurl:"./assets/music/bhaagDK.mp3",
         cardinfo:"This is the song from DK bose movie...",
+        SongNo:"4",
     },
 ];// Thus array contains the data of the trending section
 
@@ -53,37 +56,43 @@ let arrFeatured =[
         songName:"Don",
         image:"./assets/images/don.jpeg",
         Songurl:"./assets/music/maihoon.mp3",
-        cardinfo:""
+        cardinfo:"",
+        SongNo:"5",
     },
     {
         songName:"Malnag",
         image:"./assets/images/malang.jpeg",
         Songurl:"./assets/music/Malang.mp3",
-        cardinfo:""
+        cardinfo:"",
+        SongNo:"6",
     },
     {
         songName:"The Starboy",
         image:"./assets/images/satrboy.jpeg",
         Songurl:"./assets/music/Starboy.mp3",
-        cardinfo:"This song of the album Weeknd"
+        cardinfo:"This song of the album Weeknd",
+        SongNo:"7",
     },{
         songName:"Yeh Dosti",
         image:"./assets/images/sholay.jpeg",
         Songurl:"./assets/music/YehDosti.mp3",
-        cardinfo:"This song is one of the famous song of the legendry movie Sholay"
+        cardinfo:"This song is one of the famous song of the legendry movie Sholay",
+        SongNo:"8",
     },
     {
         songName:"One Love",
         image:"./assets/images/one love.jpg",
         Songurl:"./assets/music/One.mp3",
-        cardinfo:"By Shubh"
+        cardinfo:"By Shubh",
+        SongNo:"9",
     },
 ]// This array contains data of the songs in the featured section
 
 let shufflingArray = arr.concat(arrFeatured);// adding two arrays for getting the function of shuffling
 
 audio.onloadedmetadata = function(){
-    progress.max = audio.duration // maximum value of audio duration transfeered to these values
+    progress.max = audio.duration 
+    // maximum value of audio duration transfeered to these values
     progress.value = audio.currentTime;
 };//here we are updating the progress bar of the music player as the songs moves on
 
@@ -110,23 +119,24 @@ function audioPlayer(){
                 audio.play();
 }})}
 
-
+//Moving progress bar feature changing it every few millisseconds
 if(playing){
     setInterval(()=>{
         progress.value=audio.currentTime;
         songtimeUpdate()},100)
 }
-
+// function to move scrollbar on my own physically
 progress.onchange = function(){
 audio.currentTime =progress.value;   
 }
 
-
+// volume minimizing maximazing funtion by own
 function setVolume(el){
     audio.volume = el.value;
+    //using css properties and onchange adding value to recent volume of the system
 }
 
-
+//playing songon the click of the main pause play button
 document.querySelector("#plays").addEventListener("click",function(e){
     if(flag_play==0){
         e.target.src="./assets/player_pause.png";
@@ -140,6 +150,7 @@ document.querySelector("#plays").addEventListener("click",function(e){
     }
 });
 
+//Updating song time starting as well as ending
 function songtimeUpdate(){
     let curmins = Math.floor(audio.currentTime / 60);
     let cursecs = Math.floor(audio.currentTime - curmins*60);
@@ -164,13 +175,24 @@ function songtimeUpdate(){
 
 }
 
+// here I am changin the main play and pause button
+function changePlayPauseButton(){
+    if(flag_play==1){
+        audio.play();
+    }else{
+        audio.pause();
+    }
+}
+
+//Pplaying the trendibng video here
 function audioTrending(){
-    audio.src = arr[selectedSong].SongSongurl;
+    audio.src = arr[selectedSong].Songurl;
     document.querySelector(".album img").src=`${arr[selectedSong].image}`
     document.querySelector(".album-title").innerHTML=`<a href="#">${arr[selectedSong].songName}</a>
     <p class="song-name">${arr[selectedSong].cardinfo}</p>`; 
 }
 
+// Adding trending cards
 function addingTrending(){
     let clutter="";
     arr.forEach(function(details, idx){
@@ -181,10 +203,20 @@ function addingTrending(){
     </div>`;
     });
     document.querySelector(".trending").innerHTML=clutter;    
+    //selecting the tred=nding card and clling the song for trending videos
     audioTrending();
     
 }
 
+//Playing the audio od the featured seection
+function audiofeatured(){
+    audio.src = arrFeatured[selectedSong].Songurl;
+    document.querySelector(".album img").src=`${arrFeatured[selectedSong].image}`
+    document.querySelector(".album-title").innerHTML=`<a href="#">${arrFeatured[selectedSong].songName}</a>
+    <p class="song-name">${arrFeatured[selectedSong].cardinfo}</p>`;
+}
+
+//Adding cards in the recently played section
 function addingRecently(){
     let clutter="";
     recentlyAdded.forEach(function(details){
@@ -195,10 +227,9 @@ function addingRecently(){
     </div>`;
     })    
     document.querySelector(".recently").innerHTML=clutter;
-
 }
 
-
+//Shuffling the audio from all the given songs by concating the two arrays we have
 function audioShuffled(){
     audio.src = shufflingArray[selectedSong].Songurl;
     document.querySelector(".album img").src=`${shufflingArray[selectedSong].image}`
@@ -206,13 +237,7 @@ function audioShuffled(){
     <p class="song-name">${shufflingArray[selectedSong].cardinfo}</p>`;
 }
 
-function audiofeatured(){
-    audio.src = arrFeatured[selectedSong].Songurl;
-    document.querySelector(".album img").src=`${arrFeatured[selectedSong].image}`
-    document.querySelector(".album-title").innerHTML=`<a href="#">${arrFeatured[selectedSong].songName}</a>
-    <p class="song-name">${arrFeatured[selectedSong].cardinfo}</p>`;
-}
-
+// adding songs in the featured section
 function addingNearYou(){
     let clutter="";
 // ye featureing waale gane load krtA Hai pr kaam trending waale sogns pr krta hai
@@ -229,6 +254,7 @@ function addingNearYou(){
     audiofeatured()    
 }
 
+// adding liked or unliked feature 
 function heartChangeColor(){
     heart.addEventListener("click", function() {
         if (flag == 1) {
@@ -241,72 +267,24 @@ function heartChangeColor(){
     });
 }
 
-
-forward.addEventListener("click", function(){
-    if(selectedSong< arr.length-1){
-        selectedSong++;
-        audiofeatured();
-        if(flag_play==1){
-            audio.play();
-        }else{
-            audio.pause();  
-        }
-    
-        backward.style.opacity=0.7;
-        forward.addEventListener("mouseover", function(){
-            forward.style.opacity=1;
-        })
+// forwading in a way such that trending section have their own forward backward feature
+function changingSongForwardorBackward(){
+    if(arr[selectedSong].SongNo <=4){
+        audioTrending()
+        changePlayPauseButton()
+        recentlyPlayedTrending()// adding into the array of played
+        addingRecently()// showing the card in the recently played tab
     }
     else{
-        document.querySelector("#forward").style.opacity=0.2;
-    }
-})
+        console.log(arr[selectedSong].SongNo)
+        audiofeatured()
+        changePlayPauseButton();
+        recentlyPlayedFeatured()// adding it in the array of played
+        addingRecently()// showing the card in the recently played section
+    }    
+}
 
-backward.addEventListener("click",function(){
-    if(selectedSong>0 ){
-        selectedSong--;
-        addingNearYou();
-        if(flag_play==1){
-            audio.play();
-        }else{
-            audio.pause();
-        }
-        forward.style.opacity=0.7;
-        backward.addEventListener("mouseover", function(){
-            backward.style.opacity=1;
-        })
-    }
-    else{
-        document.querySelector("#backward").style.opacity=0.2;
-    }
-})
-
-
-shufffle.addEventListener("click",function(){
-    selectedSong = Math.floor((Math.random()*shufflingArray.length ));
-    audioShuffled()
-    recentlyPlayedShuffled()
-    addingRecently();
-    if(flag_play==1){
-        audio.play();
-    }else{
-        audio.pause();
-    }
-    
-})
-
-restart.addEventListener("click", function(){
-    addingNearYou();
-    
-    if(flag_play==1){
-        audio.play();
-    }else{
-        audio.pause();
-    }
-    
-})
-
-
+// adding shuffled song into the recently played song
 function recentlyPlayedShuffled(){
     if(recentlyAdded.length <4){
         recentlyAdded.unshift(shufflingArray[selectedSong])
@@ -315,18 +293,19 @@ function recentlyPlayedShuffled(){
             recentlyAdded.unshift(shufflingArray[selectedSong]);
     }
 }
-
+// pushing the song into the black array so that it can later on showed as a card for trending songs
 function recentlyPlayedTrending(){
-            if(recentlyAdded.length<4){
-                recentlyAdded.unshift(arr[selectedSong]);
+    if(recentlyAdded.length<4){
+        recentlyAdded.unshift(arr[selectedSong]);
 
-            }
-            else{
-                recentlyAdded.pop();
-                recentlyAdded.unshift(arr[selectedSong]);
-            }}
+    }
+    else{
+        recentlyAdded.pop();
+        recentlyAdded.unshift(arr[selectedSong]);
+    }
+}
         
-
+// pushing the song into the black array so that it can later on showed as a card for featured songs
 function recentlyPlayedFeatured(){
     if(recentlyAdded.length<4){
         recentlyAdded.unshift(arrFeatured[selectedSong]);
@@ -337,6 +316,60 @@ function recentlyPlayedFeatured(){
     }
 }
         
+
+// adding forward feature
+forward.addEventListener("click", function(){
+    if(selectedSong< arr.length-1){
+        selectedSong++;
+        changingSongForwardorBackward()
+        backward.style.opacity=0.7;
+        forward.addEventListener("mouseover", function(){
+            forward.style.opacity=1;
+        })
+    }
+    else{
+        document.querySelector("#forward").style.opacity=0.2;
+    }
+})
+
+// making backward feature work
+backward.addEventListener("click",function(){
+    if(selectedSong>0 ){
+        selectedSong--;
+        changingSongForwardorBackward()
+    }
+    else{
+        document.querySelector("#backward").style.opacity=0.2;
+    }
+})
+
+// Shuffling through both trending and featured songs
+shufffle.addEventListener("click",function(){
+    selectedSong = Math.floor((Math.random()*shufflingArray.length ));// selecting the shuffled song
+    audioShuffled()//  adding the card in the liked section for it 
+    recentlyPlayedShuffled()// shifting it into the array
+    addingRecently();// printing the arrayhere
+    if(flag_play==1){
+        audio.play();
+    }else{
+        audio.pause();
+    }
+    
+})
+
+// restarting song from the scratch
+restart.addEventListener("click", function(){
+    if(arr[selectedSong].SongNo <= 4){
+        audioTrending();
+        changePlayPauseButton()
+    }
+    else{
+        audiofeatured();
+        changePlayPauseButton();
+    }
+})
+
+
 audioPlayer();
 addingTrending();
 songtimeUpdate();
